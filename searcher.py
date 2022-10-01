925a254c8eae2151a468e51d49727c3ea0c97bd9
DavitShtorm <davitkocharyan1@gmail.com>
2022-10-01 12:29:50 +0400

    added browser choosing option

--- searcher.py
+++ searcher.py
@@ -1,5 +1,25 @@
 import webbrowser as wb
 
-while True:
-    search = str(input("Search: "))
-    wb.open("https://www.bing.com/search?PC=U523&q={}&pglt=2083&FORM=ANNTA1".format(search))
+
+def choose():
+    browser = input("Choose wich browser you want to use(yandex/google/bing)")
+    if browser != "yandex" and browser != "google" and browser != "bing":
+        print("Browser doesn't recognized, try again.")
+        choose()
+    elif browser == "yandex" or browser == "google" or browser == "bing":
+
+        while True:
+            search = str(input("Search: "))
+            if browser == "bing":
+                wb.open(
+                    "https://www.bing.com/search?PC=U523&q={}&pglt=2083&FORM=ANNTA1".format(search))
+            if browser == "yandex":
+                wb.open(
+                    "https://yandex.com/search/?text={}&lr=10262".format(search))
+            if browser == "google":
+                wb.open(
+                    "https://www.google.com/search?q={}".format(search))
+
+
+if __name__ == "__main__":
+    choose()
